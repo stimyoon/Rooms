69a2c8d6fbd7dcbbe76850da7a02c47e5a014e5e
stimyoon <64033604+stimyoon@users.noreply.github.com>
2022-10-02 14:53:18 -0400

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Tim Yoon on 10/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by Tim Yoon on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
